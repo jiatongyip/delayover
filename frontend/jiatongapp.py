bf0deb5ca783b6fbf8ba80077abad5ecaa39bbb4
jiatongyip <71009053+jiatongyip@users.noreply.github.com>
2022-04-09 13:36:09 +0800

    combined dep and arr model, added line plots by origin-dest, arrh, deph, carrier

--- frontend/jiatongapp.py
+++ frontend/jiatongapp.py
@@ -15,51 +15,106 @@ flask_url = 'http://127.0.0.1:5000/prediction'
   
 #reading the data needed
 airport_pairs = pd.read_csv("data/airport_pairs.csv")
+orig_dest_arr_df = pd.read_csv("data/grp_orig_dest_arr.csv")
+orig_dest_dep_df = pd.read_csv("data/grp_orig_dest_dep.csv")
+carrier_arr_df = pd.read_csv("data/grp_carrier_arr.csv")
+carrier_dep_df = pd.read_csv("data/grp_carrier_dep.csv")
+deph_arr_df = pd.read_csv("data/grp_deph_arr.csv")
+deph_dep_df = pd.read_csv("data/grp_deph_dep.csv")
+arrh_arr_df = pd.read_csv("data/grp_arrh_arr.csv")
+arrh_dep_df = pd.read_csv("data/grp_arrh_dep.csv")
+
 with open("data/options_dict.txt", "r") as file:
     options_dict = eval(file.read())
-
+with open("data/carrier_dict.txt", "r") as file:
+    carrier_dict = eval(file.read())
 # custom function
 def get_distance(orig, dest):
     dist = airport_pairs[(airport_pairs.origin_airport_code == orig) &
     (airport_pairs.dest_airport_code == dest)]['distance_grp'].values[0]
     return dist
 
-app.layout = html.Div(
-    children=[
-        html.H1("flight delay"),
-        html.H2(id='pred'),
-        html.H3("Select day of week:"),
-        dcc.RadioItems(id='dayofweek',
-            options=options_dict['dayofweek'],
-            # placeholder="Select day of week"
-            ),
-        html.H3("Select year:"),            
-        dcc.Dropdown(id='year',
-            options=options_dict['year']),
-        html.H3("Select month:"),            
-        dcc.Dropdown(id='month',
-            options=options_dict['month'],
-        #    placeholder = 'Apr'
-            ),
-        html.H3("Select departure and arrival time:"),  
-        dcc.RangeSlider(
-            min=0, max=48, step=1,
-            marks={0: '0000', 6: '0600', 12: '1200', 18: '1800',
-                24: '0000', 30: '0600', 36: '1200', 42: '1800',
-                48: '0000',},
-            value=[3, 5], allowCross = False,
-            id='time_slider'),
-        html.Div(id='time_slider_output'),
-        html.H3("Select origin:"),            
-        dcc.Dropdown(id='orig'),       
-        html.H3("Select destination:"),            
-        dcc.Dropdown(id='dest'),  
-        html.H3("Select carrier:"),            
-        dcc.Dropdown(id='carrier',
-                    options=options_dict['carrier']),                                   
-    ]
-)
 
+app.layout = html.Div([
+    html.H1('Flight delay tabs!'),
+    dcc.Tabs(
+        children=[
+            dcc.Tab(
+                label='Predicted delay',
+                children=html.Div(
+                    children=[
+                        html.H1("flight delay"),
+                        html.H2(id='pred'),
+                        html.H3("Select day of week:"),
+                        dcc.RadioItems(id='dayofweek',
+                            options=options_dict['dayofweek'],
+                            # placeholder="Select day of week"
+                            value = 1
+                            ),
+                        html.H3("Select year:"),            
+                        dcc.Dropdown(id='year',
+                            options=options_dict['year'],
+                            value = 2013
+                            ),
+                        html.H3("Select month:"),            
+                        dcc.Dropdown(id='month',
+                            options=options_dict['month'],
+                        #    placeholder = 'Apr'
+                            value = 1
+                            ),
+                        html.H3("Select departure and arrival time:"),  
+                        dcc.RangeSlider(
+                            min=0, max=48, step=1,
+                            marks={0: '0000', 6: '0600', 12: '1200', 18: '1800',
+                                24: '0000', 30: '0600', 36: '1200', 42: '1800',
+                                48: '0000',},
+                            value=[4, 24], allowCross = False,
+                            id='time_slider'),
+                        html.Div(id='time_slider_output'),
+                        html.H3("Select origin:"),            
+                        dcc.Dropdown(id='orig', value = "ATL"),       
+                        html.H3("Select destination:"),            
+                        dcc.Dropdown(id='dest', value = "ABE"),  
+                        html.H3("Select carrier:"),            
+                        dcc.Dropdown(id='carrier',
+                                    options=options_dict['carrier'])
+                    ]
+                )
+            ),
+        dcc.Tab(
+            label='Same origin and destination', 
+            children=html.Div(
+                [html.H1(id = "orig_dest_line_title"
+                    ),
+                dcc.Graph(id='orig_dest_line')]
+            )
+        ),
+        dcc.Tab(
+            label='Same carrier', 
+            children=html.Div(
+                [html.H1(id = "carrier_line_title"
+                    ),
+                dcc.Graph(id='carrier_line')]
+            )
+        ),
+        dcc.Tab(
+            label='Same departure time', 
+            children=html.Div(
+                [html.H1(id = "deph_line_title"
+                    ),
+                dcc.Graph(id='deph_line')]
+            )
+        ),
+        dcc.Tab(
+            label='Same arrival time', 
+            children=html.Div(
+                [html.H1(id = "arrh_line_title"
+                    ),
+                dcc.Graph(id='arrh_line')]
+            )
+        )        
+    ])
+])
 # callback to get prediction
 @app.callback(
     Output(component_id='pred',
@@ -79,7 +134,8 @@ app.layout = html.Div(
     component_property='value')
 )
 def get_pred(dayofweek, year, month, time_slider, orig, dest, carrier):
-    delay = "unknown"
+    arr_delay = "unknown"
+    dep_delay = "unknown"
     if all([dayofweek, year, month, time_slider, orig, dest, carrier]):
         dep , arr = time_slider
         dep = dep%24
@@ -90,7 +146,12 @@ def get_pred(dayofweek, year, month, time_slider, orig, dest, carrier):
         'origin_airport_code':orig, 'dest_airport_code': dest, 
         'distance_grp':dist}
         delay = requests.get(flask_url, params=param1).json()
-    return "your predicted delay is " + delay + " mins"
+        arr_delay = delay['arr']
+        dep_delay = delay['dep']
+
+    return ("your departure will delay by " + dep_delay + 
+    " mins,\nyour arrival will delay by " + arr_delay + " mins"
+    )
 
 # callback to display time selection
 @app.callback(
@@ -130,5 +191,134 @@ def update_orig_dd(dest):
         ].tolist()
     return sorted(orig_options)
 
+# callback for dest orig time series plot
+@app.callback(
+    Output('orig_dest_line','figure'),
+    Output('orig_dest_line_title','children'),
+    Input('orig', 'value'),
+    Input('dest', 'value')
+    )
+def update_orig_dest_plot(orig, dest):
+    # handle callback error by retrying
+    try:
+        line_plot = px.line()
+    except ValueError:
+        line_plot = px.line()
+    line_title = ""
+    if all([orig, dest]):
+        dep_subset = orig_dest_dep_df[(orig_dest_dep_df.origin_airport_code == orig) &
+        (orig_dest_dep_df.dest_airport_code == dest)]
+        dep_subset['yr_mon'] = dep_subset.yr.astype(str) + "-" + dep_subset.mon.map("{:02}".format)
+        dep_subset['type'] = "departure"
+        
+        arr_subset = orig_dest_arr_df[(orig_dest_arr_df.origin_airport_code == orig) &
+        (orig_dest_arr_df.dest_airport_code == dest)]
+        arr_subset['yr_mon'] = arr_subset.yr.astype(str) + "-" + arr_subset.mon.map("{:02}".format)
+        arr_subset['type'] = "arrival"
+        
+        df = pd.concat([dep_subset[["yr_mon", "mean", "type"]], 
+                        arr_subset[["yr_mon", "mean", "type"]]])
+        line_plot = px.line(
+            df, x = "yr_mon", y = "mean", 
+            color = "type", markers = True,
+            labels = {'yr_mon': "time", 'mean': 'Mean delay (mins)'})
+        line_title = "Let's look at the delays for flights from " + orig + " to " + dest + "!"
+
+    return line_plot, line_title
+
+@app.callback(
+    Output('carrier_line','figure'),
+    Output('carrier_line_title','children'),
+    Input('carrier', 'value'),
+    )
+def update_carrier_plot(carrier):
+    # handle callback error by retrying
+    try:
+        line_plot = px.line()
+    except ValueError:
+        line_plot = px.line()
+    line_title = ""
+    if carrier:
+        dep_subset = carrier_dep_df[carrier_dep_df.u_carrier == carrier]
+        dep_subset['yr_mon'] = dep_subset.yr.astype(str) + "-" + dep_subset.mon.map("{:02}".format)
+        dep_subset['type'] = "departure"
+        
+        arr_subset = carrier_arr_df[carrier_arr_df.u_carrier == carrier]
+        arr_subset['yr_mon'] = arr_subset.yr.astype(str) + "-" + arr_subset.mon.map("{:02}".format)
+        arr_subset['type'] = "arrival"
+        
+        df = pd.concat([dep_subset[["yr_mon", "mean", "type"]], 
+                        arr_subset[["yr_mon", "mean", "type"]]])
+        line_plot = px.line(
+            df, x = "yr_mon", y = "mean", 
+            color = "type", markers = True,
+            labels = {'yr_mon': "time", 'mean': 'Mean delay (mins)'})
+        line_title = "Let's look at the delays for flights by " + carrier_dict[carrier] + "!"
+
+    return line_plot, line_title
+
+@app.callback(
+    Output('deph_line','figure'),
+    Output('deph_line_title','children'),
+    [Input('time_slider', 'value')]
+    )
+def update_deph_plot(time_slider):
+    # handle callback error by retrying
+    try:
+        line_plot = px.line()
+    except ValueError:
+        line_plot = px.line()
+    line_title = ""
+    if time_slider:
+        dep, arr = time_slider
+        dep_subset = deph_dep_df[deph_dep_df.dep_hour == dep]
+        dep_subset['yr_mon'] = dep_subset.yr.astype(str) + "-" + dep_subset.mon.map("{:02}".format)
+        dep_subset['type'] = "departure"
+        
+        arr_subset = deph_arr_df[deph_arr_df.dep_hour == dep]
+        arr_subset['yr_mon'] = arr_subset.yr.astype(str) + "-" + arr_subset.mon.map("{:02}".format)
+        arr_subset['type'] = "arrival"
+        
+        df = pd.concat([dep_subset[["yr_mon", "mean", "type"]], 
+                        arr_subset[["yr_mon", "mean", "type"]]])
+        line_plot = px.line(
+            df, x = "yr_mon", y = "mean", 
+            color = "type", markers = True,
+            labels = {'yr_mon': "time", 'mean': 'Mean delay (mins)'})
+        line_title = "Let's look at the delays for flights departing at " + "{:02}".format(dep) + "00 hours!"
+
+    return line_plot, line_title
+
+@app.callback(
+    Output('arrh_line','figure'),
+    Output('arrh_line_title','children'),
+    [Input('time_slider', 'value')]
+    )
+def update_arrh_plot(time_slider):
+    # handle callback error by retrying
+    try:
+        line_plot = px.line()
+    except ValueError:
+        line_plot = px.line()
+    line_title = ""
+    if time_slider:
+        dep, arr = time_slider
+        dep_subset = arrh_dep_df[arrh_dep_df.arr_hour == arr]
+        dep_subset['yr_mon'] = dep_subset.yr.astype(str) + "-" + dep_subset.mon.map("{:02}".format)
+        dep_subset['type'] = "departure"
+        
+        arr_subset = arrh_arr_df[arrh_arr_df.arr_hour == arr]
+        arr_subset['yr_mon'] = arr_subset.yr.astype(str) + "-" + arr_subset.mon.map("{:02}".format)
+        arr_subset['type'] = "arrival"
+        
+        df = pd.concat([dep_subset[["yr_mon", "mean", "type"]], 
+                        arr_subset[["yr_mon", "mean", "type"]]])
+        line_plot = px.line(
+            df, x = "yr_mon", y = "mean", 
+            color = "type", markers = True,
+            labels = {'yr_mon': "time", 'mean': 'Mean delay (mins)'})
+        line_title = "Let's look at the delays for flights arriving at " + "{:02}".format(arr) + "00 hours!"
+
+    return line_plot, line_title    
 if __name__ == '__main__':
     app.run_server(debug=True)
