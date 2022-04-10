fc3cccd52c88d4d8f238ed716c2976a688893a79
wx12321 <wenxiphua@gmail.com>
2022-04-10 22:33:18 +0800

    upload file tab

--- frontend/app.py
+++ frontend/app.py
@@ -13,6 +13,13 @@ import requests
 import plotly.graph_objs as go
 from custom_functions import get_distance, get_yr_mon_dow, gen_line_plots, generate_pie_bar, update_delay_type
 
+import base64
+import datetime
+import io
+import plotly.graph_objs as go
+import cufflinks as cf
+from dash import no_update, dash_table, dcc, html
+from dash.dependencies import Input, Output, State
 app = Dash(__name__)
 flask_url = 'http://127.0.0.1:5000/prediction'
 external_stylesheets = ["https://fonts.googleapis.com/css2?family=Poppins&display=swap"]
@@ -171,7 +178,24 @@ app.layout = html.Div(
                     dcc.Graph(id='arrh_hist_delay_arr'),
                 ], style={"display":"flex","justify-content":"center","align-items":"center"})
             ])
-            )        
+            ),
+            dcc.Tab(className="custom-tab icon6", selected_className='custom-tab--selected', label='Upload files', 
+            children=html.Div([
+                html.Div(html.H1("Please upload a .csv or . xls file"),style={"font-family": 'Poppins,sans-serif'}),
+                html.Div([dcc.Upload(html.Button('Upload File'))], style={"textAlign":"center","align-items":"center"}),
+                html.Br(),
+                dcc.Upload(['Drag and Drop or ', html.A(html.B('Select a File'))], 
+                           style={
+                               'width': '100%','height': '60px',
+                               'lineHeight': '60px','borderWidth': '1px',
+                               'borderStyle': 'dashed','borderRadius': '20px',
+                               'textAlign': 'center',"align-items":"center",
+                               "display":"flex", "justify-content":"center"},
+                           multiple = False),
+                html.Div(id='output-div'),
+                html.Div(id='output-datatable')],
+                              style={"textAlign":"center","align-items":"center"})
+            )
         ])
     ]
 )
@@ -498,6 +522,100 @@ def update_arrh_pie_delay_type(time_slider):
         hist_dep, hist_arr = update_delay_type(arrh_dep_df, arrh_arr_df,
                                              [("arr_hour", arr)])
     return hist_dep, hist_arr
+
+@app.callback(Output('Mygraph', 'figure'), [
+Input('upload-data', 'contents'),
+Input('upload-data', 'filename')
+])
+def update_graph(contents, filename):
+    x = []
+    y = []
+    if contents:
+        contents = contents[0]
+        filename = filename[0]
+        df = parse_data(contents, filename)
+        df = df.set_index(df.columns[0])
+        x=df['DATE']
+        y=df['TMAX']
+    fig = go.Figure(
+        data=[
+            go.Scatter(
+                x=x, 
+                y=y, 
+                mode='lines+markers')
+            ],
+        layout=go.Layout(
+            plot_bgcolor=colors["graphBackground"],
+            paper_bgcolor=colors["graphBackground"]
+        ))
+    return fig
+
+
+def parse_data(contents, filename):
+    content_type, content_string = contents.split(",")
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if "csv" in filename:
+            # Assume that the user uploaded a CSV or TXT file
+            df = pd.read_csv(io.StringIO(decoded.decode("utf-8")))
+        elif "xls" in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+        elif "txt" or "tsv" in filename:
+            # Assume that the user upl, delimiter = r'\s+'oaded an excel file
+            df = pd.read_csv(io.StringIO(decoded.decode("utf-8")), delimiter=r"\s+")
+    except Exception as e:
+        print(e)
+        return html.Div(["There was an error processing this file."])
+
+    return df
+
+
+@app.callback(
+    Output("output-data-upload", "children"),
+    [Input("upload-data", "contents"), Input("upload-data", "filename")],
+)
+
+
+def update_table(contents, filename):
+    table = html.Div()
+
+    if contents:
+        contents = contents[0]
+        filename = filename[0]
+        df = parse_data(contents, filename)
+
+        table = html.Div(
+            [
+                html.H5(filename),
+                dash_table.DataTable(
+                    data=df.to_dict("rows"),
+                    columns=[{"name": i, "id": i} for i in df.columns],
+                ),
+                html.Hr(),
+                html.Div("Raw Content"),
+                html.Pre(
+                    contents[0:200] + "...",
+                    style={"whiteSpace": "pre-wrap", "wordBreak": "break-all"},
+                ),
+            ]
+        )
+
+    return table
+
+@app.callback(Output('output-div', 'children'),
+              Input('submit-button','n_clicks'),
+              State('stored-data','data'),
+              State('xaxis-data','value'),
+              State('yaxis-data', 'value'))
+def make_graphs(n, data, x_data, y_data):
+    if n is None:
+        return no_update
+    else:
+        bar_fig = px.bar(data, x=x_data, y=y_data)
+        # print(data)
+        return dcc.Graph(figure=bar_fig)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
