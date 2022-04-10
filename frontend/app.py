60124c6f863a79599c51889fe346919673e8718c
jiatongyip <71009053+jiatongyip@users.noreply.github.com>
2022-04-10 14:40:12 +0800

    lint

--- frontend/app.py
+++ frontend/app.py
@@ -107,153 +107,125 @@ def generate_pie_bar(dep_df, arr_df, col_list):
 
 
 # app
-app.layout = html.Div([
-    html.Div([html.H1('Flight Delay'),
-    html.Img(src=app.get_asset_url('plane.svg')),
-    html.P(html.I("Dashboard designed to predict and visualise airplane delays. You can check for flights delays that are coming from the same origin and destination , same carrier, same departure time and arrival time !"),style={"font-size":"12px","width":"650px","margin":"0 auto","padding":"10px 0"}),
-    ],style={"background-color":"#013E87","padding":"10px 0","color":"#fff","text-align":"center","font-family": 'Poppins,sans-serif',"font-size":"20px"}),
-    dcc.Tabs(className='custom-tabs-container',
-        children=[
-            dcc.Tab(className="custom-tab icon1",
-            selected_className='custom-tab--selected',
+app.layout = html.Div(
+    [
+        html.Div(
+            [html.H1('Flight Delay'),
+            html.Img(src=app.get_asset_url('plane.svg')),
+            html.P(html.I("Dashboard designed to predict and visualise airplane delays. You can check for flights delays that are coming from the same origin and destination , same carrier, same departure time and arrival time !"),style={"font-size":"12px","width":"650px","margin":"0 auto","padding":"10px 0"}),],
+            style={"background-color":"#013E87","padding":"10px 0","color":"#fff","text-align":"center","font-family": 'Poppins,sans-serif',"font-size":"20px"}
+        ),
+        dcc.Tabs(className='custom-tabs-container', children=[
+            dcc.Tab(
+                className="custom-tab icon1",
+                selected_className='custom-tab--selected',
                 label='Predicted Delay',
                 children=html.Div(
                     children=[
                         html.Div(html.H2(id='pred'), style={"font-size":"16px","padding-top":"10px"}),
-                        html.Div([
-                                html.Div(html.H2("Select Date:"),style={"padding-right":"5px"}),
-                                html.Div(
+                        html.Div(
+                            [html.Div(html.H2("Select Date:"), style={"padding-right":"5px"}),
+                            html.Div(
                                 dcc.DatePickerSingle(
-                                        id='date_picker',
-                                        min_date_allowed=date(2013, 1, 1),
-                                        max_date_allowed=date(2015, 12, 31),
-                                        initial_visible_month=date(2014, 1, 1),
-                                        date=date(2014, 1, 1),
-                                        month_format = "MMMM YYYY",
-                                        display_format = "DD-MMM-YYYY"
-                                    ),style={"padding":"10px 20px"}),
-                                html.Div(html.H2("Select Origin:"),
-                                style={"padding-right":"5px"}),
-                                html.Div(dcc.Dropdown(id='orig', value = "ATL"),
-                                style={"padding":"10px 20px","width":"100px"}),
-
-                                html.Div(html.H2("Select Destination:"),
-                                style={"padding-left":"20px","padding-right":"5px"} ),
-                                html.Div(dcc.Dropdown(id='dest', value = "ABE"),
-                                style={"padding":"10px 20px","width":"100px"}),
-
-                                html.Div(html.H2("Select Carrier:"),
-                                style={"padding-left":"20px","padding-right":"5px"}),
-                                html.Div(dcc.Dropdown(id='carrier', value = 'AA',
-                                        options=options_dict['carrier']),
-                                        style={"padding":"10px 20px","width":"200px"})
-                        ],style={"display":"flex","padding":"10px 0","align-items":"center","font-family": 'Poppins,sans-serif'}),
-
-                        html.Div(children=[
-
-                        ],style={"display":"flex","padding":"10px 0","align-items":"center","font-family": 'Poppins,sans-serif'}),
+                                    id='date_picker',
+                                    min_date_allowed=date(2013, 1, 1),
+                                    max_date_allowed=date(2015, 12, 31),
+                                    initial_visible_month=date(2014, 1, 1),
+                                    date=date(2014, 1, 1),
+                                    month_format = "MMMM YYYY",
+                                    display_format = "DD-MMM-YYYY"
+                                    ),
+                                style={"padding":"10px 20px"}
+                            ),
+                            html.Div(html.H2("Select Origin:"), style={"padding-right":"5px"}),
+                            html.Div(dcc.Dropdown(id='orig', value = "ATL"), style={"padding":"10px 20px","width":"100px"}),
+                            html.Div(html.H2("Select Destination:"), style={"padding-left":"20px","padding-right":"5px"}),
+                            html.Div(dcc.Dropdown(id='dest', value = "ABE"), style={"padding":"10px 20px","width":"100px"}),
+                            html.Div(html.H2("Select Carrier:"), style={"padding-left":"20px","padding-right":"5px"}),
+                            html.Div(dcc.Dropdown(
+                                id='carrier', value = 'AA',
+                                options=options_dict['carrier']),
+                                style={"padding":"10px 20px","width":"200px"})
+                            ], style={"display":"flex","padding":"10px 0","align-items":"center","font-family": 'Poppins,sans-serif'}
+                        ),
+                        html.Div(children=[], style={"display":"flex","padding":"10px 0","align-items":"center","font-family": 'Poppins,sans-serif'}),
                         html.H2("Select Departure and Arrival time:"), 
                         dcc.RangeSlider(
                             min=0, max=48, step=1,
-                            marks={0: '0000', 6: '0600', 12: '1200', 18: '1800',
-                                24: '0000', 30: '0600', 36: '1200', 42: '1800',
-                                48: '0000',},
-                            value=[4, 24], allowCross = False,
-                            id='time_slider'),
-                        html.Div(
-                            id='time_slider_output',style={"font-size":"17px","font-family": 'Poppins,sans-serif'}),
-
-                        
-                       
-                    ],style={"font-family": 'Poppins,sans-serif'},
+                            marks={0: '0000', 6: '0600', 12: '1200', 18: '1800', 24: '0000', 30: '0600', 36: '1200', 42: '1800', 48: '0000',},
+                            value=[4, 24], allowCross = False, id='time_slider'
+                        ),
+                        html.Div(id='time_slider_output',style={"font-size":"17px","font-family": 'Poppins,sans-serif'}),
+                    ], style={"font-family": 'Poppins,sans-serif'},
                 )
             ),
-        dcc.Tab(className="custom-tab icon2",
-        selected_className='custom-tab--selected',
-            label='Same src and dest', 
-            children=html.Div(
+            dcc.Tab(className="custom-tab icon2", selected_className='custom-tab--selected', label='Same src and dest',  children=html.Div(
                 [
-                html.H1(id = "orig_dest_line_title",style={"text-align":"center","padding-top":"10px"}),
-                dcc.Graph(id='orig_dest_line'),
-                html.H1(" Proportion of delays in 2012",style={"text-align":"center"}),
-                html.Div([
-                    dcc.Graph(id='orig_dest_pie_dep'),
-                    dcc.Graph(id='orig_dest_pie_arr'),
-
-                ],style={"display":"flex","width":"200px"}),
-                html.H1("Proportion of delays by months",style={"text-align":"center"}),
-                html.Div([
-                    dcc.Graph(id='orig_dest_bar_dep'),
-                    dcc.Graph(id='orig_dest_bar_arr'),
-
-                ],style={"display":"flex","width":"200px"})
-            ]),
-        ),
-        dcc.Tab(className="custom-tab icon3",
-        selected_className='custom-tab--selected',
-            label='Same carrier', 
-            children=html.Div([
-                html.Div(html.H1(id = "carrier_line_title"),style={"text-align":"center","padding-top":"10px"}),
+                    html.H1(id = "orig_dest_line_title", style={"text-align":"center","padding-top":"10px"}),
+                    dcc.Graph(id='orig_dest_line'),
+                    html.H1(" Proportion of delays in 2012", style={"text-align":"center"}),
+                    html.Div([
+                        dcc.Graph(id='orig_dest_pie_dep'),
+                        dcc.Graph(id='orig_dest_pie_arr'),
+                    ], style={"display":"flex","width":"200px"}
+                    ),
+                    html.H1("Proportion of delays by months", style={"text-align":"center"}),
+                    html.Div([
+                        dcc.Graph(id='orig_dest_bar_dep'),
+                        dcc.Graph(id='orig_dest_bar_arr'),
+                    ], style={"display":"flex","width":"200px"})
+                ]),
+            ),
+            dcc.Tab(className="custom-tab icon3", selected_className='custom-tab--selected', label='Same carrier', 
+            children=html.Div(
+                [html.Div(html.H1(id = "carrier_line_title"), style={"text-align":"center","padding-top":"10px"}),
                 dcc.Graph(id='carrier_line'),
-                html.Div(html.H1("Proportion of delays in 2012"),style={"text-align":"center"}),
+                html.Div(html.H1("Proportion of delays in 2012"), style={"text-align":"center"}),
                 html.Div([
                     dcc.Graph(id='carrier_pie_dep'),
                     dcc.Graph(id='carrier_pie_arr'),
-
-                ],style={"display":"flex","justify-content":"center","align-items":"center"}),
-                html.Div(html.H1("Proportion of delays by months"),style={"text-align":"center"}),
+                ], style={"display":"flex","justify-content":"center","align-items":"center"}),
+                html.Div(html.H1("Proportion of delays by months"), style={"text-align":"center"}),
                 html.Div([
                     dcc.Graph(id='carrier_bar_dep'),
                     dcc.Graph(id='carrier_bar_arr'),
-                ],style={"display":"flex","justify-content":"center","align-items":"center"})
-                ,])
-        ),
-        dcc.Tab(className="custom-tab icon4",
-        selected_className='custom-tab--selected',
-            label='Same departure time', 
+                ], style={"display":"flex","justify-content":"center","align-items":"center"}),])
+            ),
+            dcc.Tab(className="custom-tab icon4", selected_className='custom-tab--selected', label='Same departure time', 
             children=html.Div([
-                html.Div(html.H1(id = "deph_line_title"),style={"text-align":"center"}),
+                html.Div(html.H1(id = "deph_line_title"), style={"text-align":"center"}),
                 dcc.Graph(id='deph_line'),
-
                 html.Div(html.H1("Proportion of delays in 2012"),style={"text-align":"center"}),
                 html.Div([
                     dcc.Graph(id='deph_pie_dep'),
                     dcc.Graph(id='deph_pie_arr'),
-
-                ],style={"display":"flex"}),
-                html.Div(html.H1("Proportion of delays in 2012 by months"),style={"text-align":"center"}),
+                ], style={"display":"flex"}),
+                html.Div(html.H1("Proportion of delays in 2012 by months"), style={"text-align":"center"}),
                 html.Div([
                     dcc.Graph(id='deph_bar_dep'),
                     dcc.Graph(id='deph_bar_arr'),
-                    ],style={"display":"flex","justify-content":"center","align-items":"center"}),
-                ]
-            )
-        ),
-        dcc.Tab(className="custom-tab icon5",
-        selected_className='custom-tab--selected',
-            label='Same arrival time', 
+                ], style={"display":"flex","justify-content":"center","align-items":"center"}),
+            ])
+            ),
+            dcc.Tab(className="custom-tab icon5", selected_className='custom-tab--selected', label='Same arrival time', 
             children=html.Div([
-                html.Div(html.H1(id = "arrh_line_title"),style={"text-align":"center"}),
+                html.Div(html.H1(id = "arrh_line_title"), style={"text-align":"center"}),
                 dcc.Graph(id='arrh_line'),
-                
                 html.Div(html.H1("Proportion of delays in 2012"),style={"text-align":"center"}),
-
                 html.Div([
                     dcc.Graph(id='arrh_pie_dep'),
                     dcc.Graph(id='arrh_pie_arr'),
-
-                ],style={"display":"flex"}),
+                ], style={"display":"flex"}),
                 html.Div(html.H1("Proportion of delays by months"),style={"text-align":"center"}),
                 html.Div([
                     dcc.Graph(id='arrh_bar_dep'),
-                     dcc.Graph(id='arrh_bar_arr'),
-
-                ],style={"display":"flex","justify-content":"center","align-items":"center"}),
-               ]
-            )
-        )        
-    ])
-])
+                    dcc.Graph(id='arrh_bar_arr'),
+                ], style={"display":"flex","justify-content":"center","align-items":"center"}),
+            ])
+            )        
+        ])
+    ]
+)
 # callback to get prediction
 @app.callback(
     Output(component_id='pred',
@@ -286,14 +258,13 @@ def get_pred(date_picker, time_slider, orig, dest, carrier):
         dep_delay = delay['dep']
 
     return ["The departure will delay by " + dep_delay + " mins" , html.Br(),
-    "The arrival will delay by " + arr_delay + " mins"
-    ]
-
+    "The arrival will delay by " + arr_delay + " mins"]
 
 # callback to display time selection
 @app.callback(
     Output('time_slider_output', 'children'),
-    [Input('time_slider', 'value')])
+    [Input('time_slider', 'value')]
+)
 def update_output(value):
     if not value: 
         return ""
@@ -306,7 +277,8 @@ def update_output(value):
 # callback to change destination dropdown
 @app.callback(
     Output('dest','options'),
-    Input('orig','value'))
+    Input('orig','value')
+)
 def update_dest_dd(orig):
     dest_options = airport_pairs.dest_airport_code.unique()
     if orig:
@@ -318,8 +290,8 @@ def update_dest_dd(orig):
 # callback to change origin dropdown
 @app.callback(
     Output('orig','options'),
-    Input('dest','value'))
-
+    Input('dest','value')
+)
 def update_orig_dd(dest):
     orig_options = airport_pairs.origin_airport_code.unique().tolist()
     if dest:
@@ -334,7 +306,7 @@ def update_orig_dd(dest):
     Output('orig_dest_line_title','children'),
     Input('orig', 'value'),
     Input('dest', 'value')
-    )
+)
 def update_orig_dest_plot(orig, dest):
     # handle callback error by retrying
     try:
@@ -355,7 +327,7 @@ def update_orig_dest_plot(orig, dest):
     Output('orig_dest_bar_arr','figure'),   
     Input('orig', 'value'),
     Input('dest', 'value')
-    )
+)
 def update_orig_dest_pie(orig, dest):
     try:
         pie_plot_dep = px.pie()
@@ -366,8 +338,7 @@ def update_orig_dest_pie(orig, dest):
         pie_plot_dep = px.pie()
         pie_plot_arr = px.pie()
         bar_plot_dep = px.bar()
-        bar_plot_arr = px.bar()
-        
+        bar_plot_arr = px.bar() 
 
     if all([orig, dest]):
         pie_plot_dep, pie_plot_arr, bar_plot_dep, bar_plot_arr = generate_pie_bar(orig_dest_dep_df, orig_dest_arr_df, 
@@ -378,7 +349,7 @@ def update_orig_dest_pie(orig, dest):
     Output('carrier_line','figure'),
     Output('carrier_line_title','children'),
     Input('carrier', 'value'),
-    )
+)
 def update_carrier_plot(carrier):
     # handle callback error by retrying
     try:
@@ -399,7 +370,7 @@ def update_carrier_plot(carrier):
     Output('carrier_bar_dep','figure'),
     Output('carrier_bar_arr','figure'),   
     Input('carrier', 'value'),
-    )
+)
 def update_carrier_pie(carrier):
     try:
         pie_plot_dep = px.pie()
@@ -419,7 +390,7 @@ def update_carrier_pie(carrier):
     Output('deph_line','figure'),
     Output('deph_line_title','children'),
     [Input('time_slider', 'value')]
-    )
+)
 def update_deph_plot(time_slider):
     # handle callback error by retrying
     try:
@@ -485,7 +456,7 @@ def update_arrh_plot(time_slider):
     Output('arrh_bar_dep','figure'),
     Output('arrh_bar_arr','figure'),   
     [Input('time_slider', 'value')]
-    )
+)
 def update_arrh_pie(time_slider):
     try:
         pie_plot_dep = px.pie()
